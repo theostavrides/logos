2918e6a3d346988050507c0e05246605f74aaab5
Theo Stavrides <theostavrides@gmail.com>
2020-06-10 20:12:53 -0700

    first commit

---
+++ background.js
@@ -0,0 +1,40 @@
+const initContextMenus = () => {
+	chrome.contextMenus.create({
+		"id": "highlight",
+	  "title": "Highlight",
+	  "contexts": ["selection"]
+	});
+
+	chrome.contextMenus.create({
+		"id": "translate",
+	  "title": "Translate",
+	  "contexts": ["selection"]
+	});
+}
+
+const handleContextMenuClick = (item, tab) => {
+	const { selectionText, menuItemId } = item;
+	const { url, title, id } = tab;
+
+	switch(menuItemId) {
+		case 'translate':
+			chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+		    chrome.tabs.sendMessage(tabs[0].id, {action: "translate"}, console.log);  
+			});
+			break
+		case 'highlight':
+			chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+		    chrome.tabs.sendMessage(tabs[0].id, {action: "highlight"}, console.log);  
+			});
+			break
+		default:
+			return
+	}
+}
+
+const init = () => {
+	chrome.runtime.onInstalled.addListener(initContextMenus);
+	chrome.contextMenus.onClicked.addListener(handleContextMenuClick);
+}
+
+init()
---
+++ content.js
@@ -0,0 +1,57 @@
+// ------------------ RANGY ------------------------
+
+function initRangy(){
+  rangy.init();
+
+  highlighter = rangy.createHighlighter();
+
+  addHighlighterClass('yellow')
+
+  highlighter.addClassApplier(rangy.createClassApplier("rangy-highlight", {
+    ignoreWhiteSpace: true,
+    tagNames: ["span", "a"]
+  })); 
+}
+
+function addHighlighterClass(color){
+  const s = document.createElement('style');
+  const css = `.rangy-highlight { background-color: ${color} }`
+  s.setAttribute('type', 'text/css');
+
+  if ('textContent' in s) {
+    s.textContent = css;
+  } else {
+    s.styleSheet.cssText = css;
+  }
+
+  document.getElementsByTagName('head')[0].appendChild(s);
+}
+
+function highlightSelectedText() {
+  highlighter.highlightSelection("rangy-highlight");
+}
+
+function serializeSelection(){
+  console.log(rangy.serializeSelection());
+}
+
+
+// ----------------------- Messages ---------------------------
+
+const msgHandler = (message, sender, sendResponse) => { 
+	const { action } = message;
+	switch(action){
+		case 'highlight':
+			highlightSelectedText();
+      serializeSelection();
+			break;
+		case 'translate':
+			break;
+			translate();
+		default:
+			return
+	}
+}
+
+initRangy()
+chrome.runtime.onMessage.addListener(msgHandler);
